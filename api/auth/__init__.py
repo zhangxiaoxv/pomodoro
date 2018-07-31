7b07418a63de844cad3312c9d73931930bcf6075
zhangxiaoxu <zhangxiaoxv@hotmail.com>
2018-07-31 22:55:21 +0800

    pep8格式

--- api/auth/__init__.py
+++ api/auth/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify
 from flask_restful import Api, Resource
-from flask_login import login_user, logout_user, login_required, current_user
+from flask_login import login_user, logout_user, login_required
 
 from api.auth.model import User
 from api.auth.form import AuthForm
@@ -16,15 +16,17 @@ users = {'test': '123456'}
 
 @api.resource('/auth')
 class Auth(Resource):
+    @login_required
     def get(self):
         return jsonify({'result': 'success'})
 
+    @login_required
     def put(self):
         print 1111
 
     def post(self):
-        form = AuthForm()
-        if form.username.data == 'test' and form.password.data == '123456':
+        auth_form = AuthForm()
+        if auth_form.username.data == 'test' and auth_form.password.data == '123456':
             user = User()
             user.id = 'test'
             login_user(user)
@@ -36,5 +38,3 @@ class Auth(Resource):
     def delete(self):
         logout_user()
         return {}, 200
-
-
